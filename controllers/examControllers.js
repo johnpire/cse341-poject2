a96446c148682e4827870e53ba457b3faf235f50
John Carlo <ijcwasbornonthe15thofseptember@gmail.com>
2026-02-03 14:40:50 +0800

    added try/catch error handlers for all the functions

--- controllers/examControllers.js
+++ controllers/examControllers.js
@@ -3,66 +3,82 @@ const objectId = require('mongodb').ObjectId;
 
 const showExams = async (req, res, next) => {
     //#swagger.tags = ['Exams']
-    const result = await mongodb.getDatabase().db().collection('exam').find();
-    result.toArray().then((examSubjects) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('exam').find();
+        const examSubjects = await result.toArray();
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(examSubjects);
-        }).catch((err) => {
-        res.status(500).json({ message: 'Failed to retrieve exam subjects.', error: err });
-    });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred while fetching exam subjects.', error: error });
+    }
 };
 
 const createExam = async (req, res, next) => {
     //#swagger.tags = ['Exams']
-    const exam = {
-        name: req.body.name,
-        number: req.body.number,
-        email: req.body.email
-    };
-    const result = await mongodb.getDatabase().db().collection('exam').insertOne(exam);
-    if (result.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'Some error occurred while creating the exam.');
+    try {
+        const exam = {
+            name: req.body.name,
+            number: req.body.number,
+            email: req.body.email
+        };
+        const result = await mongodb.getDatabase().db().collection('exam').insertOne(exam);
+        if (result.acknowledged) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Some error occurred while creating the exam.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred while creating the exam.', error: error });
+        return;
     }
 };
 
 const updateExam = async (req, res, next) => {
-    // validator
-    if (!objectId.isValid(req.params.id)) {
-        res.status(400).json('Invalid exam ID format');
+    try {
+        // validator
+        if (!objectId.isValid(req.params.id)) {
+            res.status(400).json('Invalid exam ID format');
+            return;
+        }   
+
+        //#swagger.tags = ['Exams']
+        const examId = new objectId(req.params.id);
+        const exam = {
+            name: req.body.name,
+            number: req.body.number,
+            email: req.body.email
+        }
+        const result = await mongodb.getDatabase().db().collection('exam').replaceOne({ _id: examId }, exam);
+        if (result.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Some error occurred while updating the exam.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred while updating the exam.', error: error });
         return;
-    }   
-    
-    //#swagger.tags = ['Exams']
-    const examId = new objectId(req.params.id);
-    const exam = {
-        name: req.body.name,
-        number: req.body.number,
-        email: req.body.email
-    }
-    const result = await mongodb.getDatabase().db().collection('exam').replaceOne({ _id: examId }, exam);
-    if (result.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'Some error occurred while updating the exam.');
     }
 };
 
 const deleteExam = async (req, res, next) => {
-    // validator
-    if (!objectId.isValid(req.params.id)) {
-        res.status(400).json('Invalid exam ID format');
-        return;
-    }
+    try {
+        // validator
+        if (!objectId.isValid(req.params.id)) {
+            res.status(400).json('Invalid exam ID format');
+            return;
+        }
 
-    //#swagger.tags = ['Exams']
-    const examId = new objectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('exam').deleteOne({ _id: examId });
-    if (result.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'Some error occurred while deleting the exam.');
+        //#swagger.tags = ['Exams']
+        const examId = new objectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('exam').deleteOne({ _id: examId });
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Some error occurred while deleting the exam.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred while deleting the exam.', error: error });
+        return;
     }
 };
 
